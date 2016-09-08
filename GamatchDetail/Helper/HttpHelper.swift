158499ed78b686d5c7758daf7987984e0eb995a0
焦亚鑫 <549796162@qq.com>
2016-09-08 16:38:55 +0800

    流程草图
    10%不到

--- GamatchDetail/Helper/HttpHelper.swift
+++ GamatchDetail/Helper/HttpHelper.swift
@@ -12,10 +12,18 @@ import Alamofire
 
 class HttpHelper {
     
-    class func request(url:String, parameters: [String : AnyObject]?) {
-        Alamofire.request(.POST, url, parameters: parameters, encoding: .JSON, headers: nil)
-            .responseJSON { (response:Response) in
-                response
+    class func requestGet(url:String, parameters: [String : AnyObject]?,  completionHandler: reponse:NSHTTPURLResponse -> Void) {
+        Alamofire.request(.GET, url, parameters: parameters, encoding: .JSON, headers: nil)
+            .responseJSON { response in
+                print(response.result)
         }
     }
+    
+    class func requestPost(url:String, parameters: [String : AnyObject]?) {
+        Alamofire.request(.POST, url, parameters: parameters, encoding: .JSON, headers: nil)
+            .responseJSON { response in
+                print(response.result)
+        }
+    }
+    
 }
--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 1练习swift
 2练习db和http策略
 3顺便看一下steam dota2的api
+
+流程草图 完成度10%
+https://modao.cc/app/PJFZKQpyrJYWd9GpoRxegHiKI2KwyRJ
